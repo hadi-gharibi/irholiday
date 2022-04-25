27ab9fa0a22ac220c543254da7957b22ff0fe7ee
alg <saman.jalinous@gmail.com>
2022-04-25 13:50:32 +0430

    fix request bug - fix bug for some days - replace khayam by jdatetime
    - fix the request bug that returns just a month from time.ir
    - fix the bug for duplicate holiday dates (ex: 13th Farvardin)
    - use jdatetime as date convertor

--- irholiday/core.py
+++ irholiday/core.py
@@ -1,32 +1,41 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 from .utils import *
+import jdatetime
+
 __all__ = ['irHoliday']
+
 
 class irHoliday(object):
 
     def _main(self, start_year, end_year=None):
         if end_year is None:
             end_year = start_year
-        self.df= pd.DataFrame([],columns=['time_x', 'event_name', 'holiday', 'date'])
+        self.df = pd.DataFrame([], columns=['time_x', 'event_name', 'holiday', 'date'])
 
-        for year in range(start_year, end_year+1):
+        for year in range(start_year, end_year + 1):
             for month in range(1, 13):
                 POSTstr = "Year={}&Month={}&Base1=0&Base2=1&Base3=2&Responsive=true".format(year, month)
                 r = req(POSTstr)
                 soup = soup_obj(r)
                 temp = month_data_extractor(soup)
                 temp['date'] = temp.time_x.apply(text_jalali_to_date, args=[year])
-                self.df = self.df.append(temp.drop_duplicates())
+                self.df = pd.concat([self.df,temp.drop_duplicates()])
                 self.df.dropna(inplace=True)
 
-        self.df['jalali_date'] = self.df['date'].apply(lambda date: khayyam.JalaliDate((date)))
+        self.df['jdate_time'] = self.df['date'].apply(lambda date: jdatetime.date.fromgregorian(date=date))
 
         return self.df.reset_index(drop=True)
 
     def to_df(self, start_year, end_year=None):
         self._main(start_year, end_year)
         return self.df
+
+    def get_holidays(self, start_year, end_year=None):
+        self._main(start_year, end_year)
+        df_holiday = self.df[self.df['holiday'] == 1].reset_index()
+        del df_holiday['index']
+        return df_holiday
 
     def to_csv(self, start_year, end_year=None, path='irholidays.csv'):
         self._main(start_year, end_year)
--- irholiday/utils.py
+++ irholiday/utils.py
@@ -1,46 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
-import khayyam
 import json
 import pandas as pd
 import re
+import convert_numbers
+import jdatetime
+
+month_list = ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'اَمرداد', 'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن',
+              'اسفند']
+
+def holiday_correction(df):
+    indexes = df.index[df['time_x'] == 0].tolist()
+    for index in indexes:
+        correct_value = df.iloc[index]['time_y'].strip('\n')
+        wrong_value = df.iloc[index]['time_x']
+        df["time_x"].replace({wrong_value: correct_value}, inplace=True)
+
 
 def req(POSTstr):
-
-    headers={
-        'Host': 'www.time.ir',
-        'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0',
-        'Accept': '*/*',
-        'Accept-Language': 'en-GB,en;q=0.5',
-        'Accept-Encoding': 'gzip, deflate',
-        'Referer': 'http://www.time.ir/',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'X-Requested-With': 'XMLHttpRequest',
-        'Content-Length': '57',
-        'Connection': 'keep-alive'
-    }
-
-    url='https://www.time.ir'
-    r = requests.post(url=url,data=json.dumps(POSTstr),headers=headers)
+    url = 'https://www.time.ir'
+    r = requests.post(url=url+'?'+POSTstr)
     return r
 
 
 def soup_obj(r):
-
     soup_obj = BeautifulSoup(r.text, 'html.parser')
     return soup_obj
 
 
 def month_data_extractor(soup):
-
     events = soup.find('ul', attrs={'class': 'list-unstyled'})
     a1 = df_maker(events_month_seprator(events))
     holi = holiday_extractor(events)
     holi = '\n\n\n'.join(x.text for x in holi)
     a2 = df_maker(pd.DataFrame(events_month_seprator(holi)))
-    df = a1.merge(a2,how='left',on='event_name').fillna(0)
-    df.loc[df.time_y != 0 ,'time_y'] = 1
-    df.rename(columns={'time_y':'holiday'},inplace=True)
+    if a2.iloc[0]['event_name'] is None:
+        df = a1.copy()
+        df['time_y'] = 0
+        df.rename(columns={'time': 'time_x'}, inplace=True)
+    else:
+        df = a1.merge(a2, how='outer', on='event_name').fillna(0)
+        holiday_correction(df)
+    df.loc[df.time_y != 0, 'time_y'] = 1
+    df.rename(columns={'time_y': 'holiday'}, inplace=True)
     return df
 
 
@@ -51,35 +53,30 @@ def df_maker(df):
 
 
 def holiday_extractor(events):
-    return events.find_all('li',attrs={'class': 'eventHoliday'})
+    return events.find_all('li', attrs={'class': 'eventHoliday'})
 
 
 def text_jalali_to_date(text, year):
-    translation_dest = '0123456789'
-    translation_src = '۰۱۲۳۴۵۶۷۸۹'
-    translations = maketrans(translation_src, translation_dest)
     text = text.split(' ')
-    day = int(text[0].translate(translations))
+    day = int(convert_numbers.persian_to_english(text[0]))
     month = text[1]
-    month_list = ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'اَمرداد', 'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند']
     month = month_list.index(month) + 1
     try:
-        return khayyam.jalali_date.JalaliDate(year, month, day).todate()
+        return jdatetime.date(year, month,day).togregorian()
     except:
         return None
+
 
 def events_month_seprator(events):
     try:
         events = events.text
     except:
         pass
-    events_list =  re.sub(' +',' ',events.strip()).split('\n\n\n')
-    events_list = [event.split('\r\n',maxsplit=1) for event in events_list]
-    for i,event in enumerate(events_list):
-        for j,x in enumerate(event):
-            events_list[i][j] = x.replace('\r\n' ,'').replace('\n\n\n','')
+    events_list = re.sub(' +', ' ', events.strip()).split('\n\n\n')
+    events_list = [event.split('\r\n', maxsplit=1) for event in events_list]
+    for i, event in enumerate(events_list):
+        for j, x in enumerate(event):
+            events_list[i][j] = x.replace('\r\n', '').replace('\n\n\n', '')
             if j == 1: events_list[i][j] = events_list[i][j][1:]
-    return [['time','event_name']]+events_list
-
-maketrans = lambda A, B: dict((ord(a), b) for a, b in zip(A, B))
+    return [['time', 'event_name']] + events_list
 
